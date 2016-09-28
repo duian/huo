a4d067a45ae24fe906c6b56fd3e86c5e27426be8
miss-taken <wangxinghua0519@163.com>
2016-09-28 15:55:13 +0800

    fix

--- src/views/MyCargoDetail/index.js
+++ src/views/MyCargoDetail/index.js
@@ -171,7 +171,7 @@ class CargoDetail extends React.Component {
         signType,
         paySign,
         outTradeNo,
-      } = resultData.result;
+      } = returnData.result;
       wx.config({
         // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         debug: false,
