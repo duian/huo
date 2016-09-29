61c5ff31d7d047d8c957ed7dadc0311e4df27d66
duian <zhouqiao0418@gmail.com>
2016-09-29 20:58:56 +0800

    feat: web.js代码优化

--- src/utils/web.js
+++ src/utils/web.js
@@ -2,25 +2,24 @@ import url from './url';
 import request from 'superagent-bluebird-promise';
 
 
-export function postRequest(data,serviceName,success,fail){
-   const uuid = localStorage.getItem('uuid');
-
-   const params = {
-        data: data,
-        service: serviceName,
-        uuid,
-        timestamp: '',
-        signatures: '',
-      };
-   request.post(url.webapp)
-   .withCredentials()
-   .send(params)
-   .then((res) => { 
-      const returnData = JSON.parse(res.text);
-      if (returnData.success) {
-        success(returnData);
-      } else {
-        fail(returnData);
-      }
-    });
+export function postRequest(data, serviceName, success, fail) {
+  const uuid = localStorage.getItem('uuid');
+  const params = {
+    data,
+    service: serviceName,
+    uuid,
+    timestamp: '',
+    signatures: '',
+  };
+  return request.post(url.webapp)
+  .withCredentials()
+  .send(params)
+  .then((res) => {
+    const returnData = JSON.parse(res.text);
+    if (returnData.success) {
+      success(returnData);
+    } else {
+      fail(returnData);
+    }
+  });
 }
